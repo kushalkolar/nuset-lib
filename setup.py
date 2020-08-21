3ad904e5ce2fb5ca0e64634d313e7795d5af5844
kushalkolar <kushalkolar@gmail.com>
2020-08-21 22:26:28 +0200

    create setup.py

---
+++ setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+
+
+install_requires = \
+    [
+        "numpy", # newer numpy won't work with scikit-learn 0.13
+        "scikit-image~=0.15.0", # newer scikit-image don't have the `min_size` kwarg for skimage.morphology.remove_small_holes
+        "Pillow",
+        "tqdm"
+    ]
+
+classifiers = \
+    [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX :: Linux",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+        "Topic :: Scientific/Engineering :: Image Recognition",
+        "Topic :: Scientific/Engineering :: Image Processing",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Intended Audience :: Science/Research",
+    ]
+
+with open("README.md", 'r') as fh:
+    long_description = fh.read()
+
+setup(
+    name='nuset',
+    version='0.1.0',
+    packages=find_packages(),
+    include_package_data=True,
+    url='',
+    license='MIT License',
+    description='',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    classifiers=classifiers,
+    install_requires=install_requires
+)
