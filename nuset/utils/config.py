e7a3a0db4c0f62c21359b79a540506e67acf19cc
kushalkolar <kushalkolar@gmail.com>
2020-08-21 02:50:43 +0200

    download func done

--- nuset/utils/config.py
+++ nuset/utils/config.py
@@ -1,5 +1,6 @@
 import os
-import wget
+import requests
+import tqdm
 
 
 if os.name == 'nt':
@@ -22,7 +23,7 @@ if not os.path.isdir(DEFAULT_NETWORK_DIR):
     os.makedirs(DEFAULT_NETWORK_DIR, exist_ok=True)
 
 # default network files
-_default_network_fnames = \
+default_network_fnames = \
     [
         'foreground.ckpt.data-00000-of-00001',
         'whole_norm.ckpt.meta',
@@ -33,11 +34,26 @@ _default_network_fnames = \
     ]
 
 
-def _download_network_file(fname: str):
+def download_network_file(fname: str):
     print('Downloading default network file')
-    wget.download(f'https://zenodo.org/record/<RECORD_ID>/files/{fname}')
+    url = f'https://zenodo.org/record/<RECORD_ID>/files/{fname}'
+
+    # basically from https://stackoverflow.com/questions/37573483/progress-bar-while-download-file-over-http-with-requests/37573701
+    response = requests.get(url, stream=True)
+    total_size_in_bytes = int(response.headers.get('content-length', 0))
+    block_size = 1024  # 1 Kibibyte
+    progress_bar = tqdm(total=total_size_in_bytes, unit='iB', unit_scale=True)
+
+    with open('test.dat', 'wb') as file:
+        for data in response.iter_content(block_size):
+            progress_bar.update(len(data))
+            file.write(data)
+    progress_bar.close()
+
+    if total_size_in_bytes != 0 and progress_bar.n != total_size_in_bytes:
+        raise IOError(f'Could not download the following network file: {fname}')
 
 
-for _f in _default_network_fnames:
-    if not os.path.isfile(_f):
-        _download_network_file(_f)
+for _f in default_network_fnames:
+    if not os.path.isfile(os.path.join(DEFAULT_NETWORK_DIR, _f)):
+        download_network_file(_f)
