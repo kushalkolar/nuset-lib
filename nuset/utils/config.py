af99b6f25470e86c4da42c3399a835db1f050dfb
kushalkolar <kushalkolar@gmail.com>
2020-08-26 06:50:35 +0200

    finished config for downloading the large network files

--- nuset/utils/config.py
+++ nuset/utils/config.py
@@ -1,6 +1,9 @@
 import os
 import requests
 import tqdm
+from warnings import warn
+from shutil import copy
+from .. import network
 
 
 if os.name == 'nt':
@@ -17,26 +20,33 @@ if not os.path.isdir(DIR):
     os.makedirs(DIR, exist_ok=True)
 
 # store the default network files here
-DEFAULT_NETWORK_DIR = os.path.join(DIR, 'networks', 'default')
+DEFAULT_NETWORK_DIR = os.path.join(DIR, 'networks', 'nuset_default')
 
 if not os.path.isdir(DEFAULT_NETWORK_DIR):
     os.makedirs(DEFAULT_NETWORK_DIR, exist_ok=True)
 
 # default network files
-default_network_fnames = \
+large_network_fnames = \
     [
         'foreground.ckpt.data-00000-of-00001',
-        'whole_norm.ckpt.meta',
         'whole_norm.ckpt.data-00000-of-00001',
-        'foreground.ckpt.index',
-        'foreground.ckpt.meta',
-        'whole_norm.ckpt.index'
     ]
+
+small_network_files = \
+[
+    'foreground.ckpt.index',
+    'foreground.ckpt.meta',
+    'whole_norm.ckpt.index',
+    'whole_norm.ckpt.meta',
+]
 
 
 def download_network_file(fname: str):
+    """
+    Download the large network files from Zenodo
+    """
     print('Downloading default network file')
-    url = f'https://zenodo.org/record/<RECORD_ID>/files/{fname}'
+    url = f'https://zenodo.org/record/3996370/files/{fname}'
 
     # basically from https://stackoverflow.com/questions/37573483/progress-bar-while-download-file-over-http-with-requests/37573701
     response = requests.get(url, stream=True)
@@ -44,16 +54,29 @@ def download_network_file(fname: str):
     block_size = 1024  # 1 Kibibyte
     progress_bar = tqdm(total=total_size_in_bytes, unit='iB', unit_scale=True)
 
-    with open('test.dat', 'wb') as file:
+    with open(os.path.join(DEFAULT_NETWORK_DIR, fname), 'wb') as file:
         for data in response.iter_content(block_size):
             progress_bar.update(len(data))
             file.write(data)
     progress_bar.close()
 
     if total_size_in_bytes != 0 and progress_bar.n != total_size_in_bytes:
-        raise IOError(f'Could not download the following network file: {fname}')
+        raise warn(f'Could not download the following network file: {fname}\n'
+                   f'NuSeT features will not be functional. You can try downloading'
+                   f' the file again.')
 
 
-for _f in default_network_fnames:
+def move_network_file(fname):
+    """
+    Move the small network files from the repo to the config dir
+    """
+    basepath = os.path.dirname(network.__file__)
+    copy(
+        os.path.join(basepath, fname),
+        os.path.join(DEFAULT_NETWORK_DIR, fname)
+    )
+
+
+for _f in large_network_fnames:
     if not os.path.isfile(os.path.join(DEFAULT_NETWORK_DIR, _f)):
         download_network_file(_f)
